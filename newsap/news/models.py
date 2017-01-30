710b91b635ce4cfb1770a809e8e5d4c63b8ad839
akshay karapurkar <karapurkar.akshay@gmail.com>
2017-01-30 11:46:14 +0530

    updated models

--- newsap/news/models.py
+++ newsap/news/models.py
@@ -4,41 +4,41 @@ from django.db import models
 
 # Create your models here.
 
-class resolved_news_type(models.Model):
+class ResolvedNewsType(models.Model):
 	resolved_news_type_name=models.CharField(max_length=250, unique=True)
 
-class unresolved_news_type(models.Model):
+class UnresolvedNewsType(models.Model):
 	unresolved_news_type_name=models.CharField(max_length=250)
 
-class map_unresolved_resolved_news_type_map(models.Model):
+class MapUnresolvedResolvedNewsType_map(models.Model):
 	resolved_news_type_id=models.ForeignKey(resolved_news_type)
 	unresolved_news_type_id=models.ForeignKey(unresolved_news_type,unique=True)
 
-class resolved_location(models.Model):
+class ResolvedLocation(models.Model):
 	resolved_location_name=models.CharField(max_length=250,unique=True)
 
-class unresolved_location(models.Model):
+class UnresolvedLocation(models.Model):
 	unresolved_location_name=models.CharField(max_length=250)
 
-class map_unresolved_resolved_location(models.Model):
+class MapUnresolvedResolvedLocation(models.Model):
 	resolved_location_id=models.ForeignKey(resolved_location)
 	unresolved_location_id=models.ForeignKey(unresolved_location,unique=True)
 
-class source(models.Model):
+class Source(models.Model):
 	source_name=models.CharField(max_length=250)
 	source_url=models.URLField("URL", unique=True)
 
-class author(models.Model):
+class Author(models.Model):
 	author_name=models.CharField(max_length=250)
 	source_id=models.ForeignKey(source)
 
-class article_download(models.Model):
+class ArticleDownload(models.Model):
 	local_file_path=models.CharField(max_length=500)
 	article_download_created_date=models.DateTimeField(blank=False)
 	article_download_last_updated=models.DateTimeField(blank=False)
 	iss_parsed=models.TextField(max_length=5)
 
-class article_parsed(models.Model):
+class ArticleParsed(models.Model):
 	article_title=models.CharField(max_length=1000)
 	uresolved_news_type_id=models.ForeignKey(unresolved_news_type)
 	url=models.URLField("URL", unique=True)
@@ -52,7 +52,7 @@ class article_parsed(models.Model):
 
 
 
-class article_content(models.Model):
+class ArticleContent(models.Model):
 	article_parsed_id=models.ForeignKey(article_parsed)
 	content=models.TextField()
 
